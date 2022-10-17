a8fc11ad8552f50a783d93ae21baf1f849fd7692
Ali NIZAM <ali.nizam@fsm.edu.tr>
2022-10-17 13:48:43 +0300

    Week 4

---
+++ Lesson4Application1/Lesson4Application1/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lesson4Application1.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
---
+++ Lesson4Application1/Lesson4Application1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson4Application1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
